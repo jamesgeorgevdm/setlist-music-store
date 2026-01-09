4d7adbfa05a1d9b700aa7f37055342db1c3b79c5
jamesgeorgevdm <jamesv234@gmail.com>
2026-01-09 13:45:46 +0200

    Changed MySQL to PostgreSQL for Render

--- setlist/settings.py
+++ setlist/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = "setlist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
 }
 
