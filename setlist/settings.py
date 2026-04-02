7a69a7bfd5f1ee6671753ec26e145b285e41cad8
jamesgeorgevdm <jamesv234@gmail.com>
2026-04-02 16:42:10 +0200

    Updated env variables DATABASE_URL

--- setlist/settings.py
+++ setlist/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import ssl
+import dj_database_url
 
 
 load_dotenv()  # load variables from .env
@@ -92,19 +93,13 @@ WSGI_APPLICATION = "setlist.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
+    'default': dj_database_url.config(
+        # This looks for the DATABASE_URL environment variable we just set
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 
 # Password validation
